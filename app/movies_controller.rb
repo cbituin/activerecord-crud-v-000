244bb352abf531c83517ac9325209b1f6edbf835
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-05-26 05:51:12 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -35,7 +35,6 @@ def can_be_created_in_a_block(args = {title: "Home alone", release_date: "1990"}
     m.release_date = args.release_date
     m.save
   end
-  m
 end
 
 def can_get_the_first_item_in_the_database
